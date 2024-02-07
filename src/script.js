5f4206ed7cf7c8a2b34d2aec2c788fc7e45ce0da
baronjada <jadachantellebaron@gmail.com>
2024-02-07 11:48:47 +0000

    Added functionality to searchCity function to allow for the h1 inner html to be replaced by the value which the user enters into the search form input

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
 function searchCity(event) {
   event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let weatherAppCity = document.querySelector("#weather-app-city");
+  weatherAppCity.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
