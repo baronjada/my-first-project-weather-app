418b713d6e5a65f1baee51ff65d2dcaf8a81ad35
baronjada <jadachantellebaron@gmail.com>
2024-02-07 12:17:51 +0000

    Added displayTeperature function to show correct current temperature pulled from API and inserting it into temperature element on the page - not yet based on city inputted into search box, info pulling based on city variable

--- src/script.js
+++ src/script.js
@@ -5,5 +5,17 @@ function searchCity(event) {
   weatherAppCity.innerHTML = searchInput.value;
 }
 
+function displayTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#weather-app-temperature");
+  temperatureElement.innerHTML = temperature;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchCity);
+
+let city = "London";
+let apiKey = "7789b9fafffta3o538f2b30e883d3734";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);
