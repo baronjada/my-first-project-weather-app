217d25ae560b7b0106c5bd343d0d8a17353583cb
baronjada <jadachantellebaron@gmail.com>
2024-02-07 12:28:39 +0000

    Amended function names and added searchCity function which will use the value (city name) entered into the search input box by the user, to insert into and console log an apiUrl that displays correct temperature data for that specified city

--- src/script.js
+++ src/script.js
@@ -1,21 +1,23 @@
-function searchCity(event) {
+function searchCity(city) {
+  let apiKey = "7789b9fafffta3o538f2b30e883d3734";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
+function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let weatherAppCity = document.querySelector("#weather-app-city");
   weatherAppCity.innerHTML = searchInput.value;
-}
-
-function displayTemperature(response) {
-  let temperature = Math.round(response.data.temperature.current);
-  let temperatureElement = document.querySelector("#weather-app-temperature");
-  temperatureElement.innerHTML = temperature;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", searchSubmit);
 
-let city = "London";
-let apiKey = "7789b9fafffta3o538f2b30e883d3734";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+// axios.get(apiUrl).then(displayTemperature);
 
-axios.get(apiUrl).then(displayTemperature);
+// function displayTemperature(response) {
+//   let temperature = Math.round(response.data.temperature.current);
+//   let temperatureElement = document.querySelector("#weather-app-temperature");
+//   temperatureElement.innerHTML = temperature};
